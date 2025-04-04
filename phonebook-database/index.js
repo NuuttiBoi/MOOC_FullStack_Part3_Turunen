1ef285cf2307555989e266abbb4ec1295de8ab8b
NuuttiBoi <104026912+NuuttiBoi@users.noreply.github.com>
2025-04-04 19:14:36 +0300

    name validation

--- phonebook-database/index.js
+++ phonebook-database/index.js
@@ -8,6 +8,8 @@ const errorHandler = (error, request, response, next) => {
     console.error(error.message)
     if(error.name === 'CastError'){
         return response.status(400).send({error:'malformatted id'})
+    } else if(error.name === 'ValidationError'){
+        return response.status(400).send({error: error.message})
     }
     next(error)
 }
@@ -66,13 +68,14 @@ app.delete('/api/persons/:id',(request,response) =>{
 
         })
 })
-app.post('/api/persons', (request, response) =>{
+app.post('/api/persons', (request, response, next) =>{
     const body = request.body
     if(!body.name || !body.number){
         return response.status(400).json({
             error: 'missing name and/or number'
         })
-    } else if((persons.filter(person =>
+    }
+    else if((persons.filter(person =>
         person.name.toLowerCase()=== body.name.toLowerCase())).length>0){
         return response.status(400).json({
             error: 'name already exists in the phonebook'
@@ -82,9 +85,12 @@ app.post('/api/persons', (request, response) =>{
             name: request.body.name,
             number: request.body.number,
         })
-        person.save().then(savedPerson => {
-            response.json(savedPerson)
-        })
+        person.save()
+            .then(savedPerson => {
+            response.json(savedPerson)})
+            .catch(error => {
+                next(error)
+            })
     }
 })
 
