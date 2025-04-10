4c57e00d57fca35b30334d9f48069a4aa724710a
NuuttiBoi <104026912+NuuttiBoi@users.noreply.github.com>
2025-04-10 17:20:55 +0300

    render scripts

--- phonebook-database-fullstack/index.js
+++ phonebook-database-fullstack/index.js
@@ -113,7 +113,7 @@ app.use(unknownEndpoint)
 
 app.use(errorHandler)
 
-const PORT= process.env.PORT
+const PORT= process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`server running on port ${PORT}`)
 })
